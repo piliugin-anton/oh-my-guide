23445e73e8585dc6b3ae24ca570bfa6b0e722833
Anton Piliugin <anton.piliugin@icloud.com>
2024-05-06 12:54:05 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <div class="column">
+      <div class="row" v-for="i in getRandomInt(100, 150)">
+        <Cube v-for="i in getRandomInt(10, 15)" />
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import Cube from './components/Cube.vue'
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+</script>
+
+<style lang="scss" scoped>
+.column {
+  display: flex;
+  flex-direction: column;
+}
+
+.row {
+  display: flex;
+  flex-direction: row;
+
+  & > * {
+    margin: 4px;
+  }
+}
+</style>
---
+++ src/components/Cube.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cube"></div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.cube {
+  width: 64px;
+  height: 64px;
+  background-color: orange;
+  border-radius: 16px;
+  border: 2px solid #888;
+  transition: transform 0.333s ease-in-out;
+
+  &:hover {
+    transform: scale(0.8, 0.8);
+  }
+}
+</style>
